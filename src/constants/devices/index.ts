cbbe7b1af05b2e65787b7be5806a592a17009285
Vlad Cuciureanu <vlad.cuciureanu@pm.me>
2023-06-14 15:01:13 +0300

    refactor: Removed duplicate icons and mapped MX Master 3/3S

--- src/constants/devices/index.ts
+++ src/constants/devices/index.ts
@@ -1,18 +1,20 @@
 import AppleDevices from "./apple";
 import BoseDevices from "./bose";
 import GoogleDevices from "./google";
+import JabraDevices from "./jabra";
+import LogitechDevices from "./logitech";
 import SamsungDevices from "./samsung";
 import SonyDevices from "./sony";
 import UgreenDevices from "./ugreen";
-import JabraDevices from "./jabra";
 import { DeviceDefinition } from "src/types/device";
 
 export const DevicesMap: Record<string, Record<string, DeviceDefinition>> = {
   "0x004C": AppleDevices,
   "0x009E": BoseDevices,
   "0x00E0": GoogleDevices,
+  "0x0067": JabraDevices,
+  "0x046D": LogitechDevices,
   "0x0075": SamsungDevices,
   "0x054C": SonyDevices,
   "0x005D": UgreenDevices,
-  "0x0067": JabraDevices,
 };
--- src/constants/devices/sony.ts
+++ src/constants/devices/sony.ts
@@ -7,11 +7,11 @@ export default {
   },
   "0x0D58": {
     name: "WH-1000XM4",
-    main: "icons/devices/sony/wh.1000xm4.svg",
+    main: "icons/devices/sony/wh.1000xm3-4.svg",
   },
   "0x0CD3": {
     name: "WH-1000XM3",
-    main: "icons/devices/sony/wh.1000xm3.svg",
+    main: "icons/devices/sony/wh.1000xm3-4.svg",
   },
   "0x0DE1": {
     name: "WF-1000XM4",
