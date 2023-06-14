9629818d9c98187c31ac8000fed1ce01fee96804
Vlad Cuciureanu <vlad.cuciureanu@pm.me>
2023-06-14 13:08:35 +0300

    feat: Sony WH-1000XM3/4/5 and Bose QC35 I/II

--- src/constants/devices/bose.ts
+++ src/constants/devices/bose.ts
@@ -1,6 +1,13 @@
 const devices: Record<string, Record<string, string>> = {
-  // QC35II
-  "0x4020": {},
+  // QC35 II
+  "0x4020": {
+    main: "icons/devices/bose/qc.35.svg",
+  },
+  // QC35
+  "0x400C": {
+    main: "icons/devices/bose/qc.35.svg",
+
+  }
 };
 
 export default devices;
--- src/constants/devices/sony.ts
+++ src/constants/devices/sony.ts
@@ -1,8 +1,16 @@
 const devices: Record<string, Record<string, string>> = {
+  // WH-1000XM5
+  "0x0DF0": {
+    main: "icons/devices/sony/wh.1000xm5.svg",
+  },
   // WH-1000XM4
-  "0x0D58": {},
+  "0x0D58": {
+    main: "icons/devices/sony/wh.1000xm4.svg",
+  },
   // WH-1000XM3
-  "0x0CD3": {},
+  "0x0CD3": {
+    main: "icons/devices/sony/wh.1000xm3.svg",
+  },
   // WF-1000XM4
   "0x0DE1": {
     main: "icons/devices/sony/wf.1000xm4.svg",
